85d92a53de93b64f5aecbebec4b2f330922e1259
johngiangrossi <john.giangrossi@email.com>
2025-07-03 19:47:55 +0200

    ADD ex-6

---
+++ src/App6.java
@@ -0,0 +1,17 @@
+public class App6 {
+    public static void main(String[] args) {
+        /*6) 
+        Inverti l’array
+        Obiettivo: Stampa gli elementi dell’array in ordine inverso.
+        Esempio: int [] a = {1, 2, 3, 4, 5}
+        Output: 5, 4, 3, 2, 1 */
+
+        int[] a = { 1, 2, 3, 4, 5};
+
+        for (int i = (a.length - 1); i > 0; i--) {
+            int result = a[i];
+
+            System.out.println(result);
+        }
+    }
+}
